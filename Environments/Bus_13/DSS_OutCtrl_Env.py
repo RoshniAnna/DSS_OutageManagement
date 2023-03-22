09d86a7703f9b13eada644d3b4d3ac477c4483dd
Steve Paul Muttath <39541194+iamstevepaul@users.noreply.github.com>
2023-03-22 00:12:34 -0400

    Clean up and reorgnize

---
+++ Environments/Bus_13/DSS_OutCtrl_Env.py
@@ -0,0 +1,97 @@
+import gym
+import random
+import win32com.client
+from gym import spaces
+import numpy as np
+import logging
+from random import sample,choices,uniform, randint
+from math import ceil
+from Environments.Bus_13.DSS_Initialize import * 
+from Environments.Bus_13.state_action_reward import *
+from gym.utils import seeding
+
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.WARNING)
+
+class DSS_OutCtrl_Env(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):        
+        print("Initializing 13-bus env for outage management with Generators, sectionalizing and tie switches")
+        self.DSSCktObj,G_init,conv_flag=initialize() # the DSSCircuit is set up and initialized
+        # Set up action and observation space variables
+        n_actions=len(sectional_swt)+len(tie_swt) # the switching actions 
+        self.outedges=[] # to track the outage conditions
+        # self.outtime=0 # Not considered in training
+        self.G=G_init.copy() 
+        self.action_space = spaces.MultiBinary(n_actions)
+        self.observation_space=spaces.Dict({"EnergySupp":spaces.Box(low=0, high=2,shape=(1,)),
+            "NodeFeat(BusVoltage)":spaces.Box(low=0, high=2, shape=(len(G_init.nodes()),3)),
+            "EdgeFeat(Branchflow)":spaces.Box(low=0, high=2,shape=(len(G_init.edges()),)),
+            "Adjacency":spaces.Box(low=0, high=1,shape=(len(G_init.nodes()),len(G_init.nodes()))),
+            "VoltageViolation":spaces.Box(low=0, high=1000,shape=(1,)),
+            "ConvergenceViolation":spaces.Box(low=0, high=1,shape=(1,)),
+            "ActionMasking":spaces.Box(low=0, high=1, shape=(n_actions,))
+            })
+        print('Env initialized')
+        
+
+    def step(self, action):
+        # Getting observation before action is executed
+        observation = get_state(self.DSSCktObj,self.G,self.outedges) #function to get state of the network   
+        # Executing the switching action
+        self.DSSCktObj, self.G =take_action(action, self.outedges) #function to implement the action      
+        #Getting observation after action is taken
+        obs_post_action = get_state(self.DSSCktObj,self.G,self.outedges)
+        reward = get_reward(obs_post_action) #function to calculate reward
+        done = True
+        info = {"is_success": done,
+                    "episode": {
+                        "r": reward,
+                        "l": 1
+                    }
+                    }
+        logging.info('Step success')
+        return obs_post_action, reward[0], done, info
+
+
+    def reset(self):
+        # In reset function we simulate different line outage scenarios
+        logging.info('resetting environment...')
+        self.DSSCktObj,G_init,conv_flag=initialize() #initial set up
+        self.G=G_init.copy()
+        #Subgraph approach for outages
+        max_rad=nx.diameter(G_init) # maximum radius to create a subgraph
+        max_percfail=0.5 # Percentage of edges failed within subgraph -variable
+        nd=random.choice(list(G_init.nodes())) #select initial node around which subgraph is formed
+        rad=ceil(uniform(0, max_rad)) # select radius of subgraph
+        
+        # Failure isloation simulation do using base graph
+        Gsub=nx.ego_graph(G_base, nd , radius=rad, undirected=False) #form subgraph around selected node with given radius
+        sub_edges=list(Gsub.edges()) # list of subgraph edges
+        out_perc = uniform(0, max_percfail) # percentage of edge failure within subgraph
+        N_out=math.ceil(len(sub_edges)*out_perc) # number of edge outages within subgraph
+        out_edges=sample(sub_edges,k=N_out) # random sampling without replacement # random failure of edges within subgraph
+        
+        # if len(out_edges)==0: #outage time is ignored
+        #     out_time=0
+        # else:
+        #     out_time= randint(1, 24) # randomly select the time of outage (From 1 to end of time period) 
+        
+        for o_e in out_edges:
+            (u,v)=o_e
+            branch_name= G_init.edges[o_e]['label'][0]
+            self.DSSCktObj.dssText.command=(branch_name + '.enabled="False"')
+            self.DSSCktObj.dssSolution.Solve() 
+        self.G.remove_edges_from(out_edges)
+        self.outedges=out_edges
+        # self.outtime =out_time
+        logging.info("reset complete\n")
+        obs = get_state(self.DSSCktObj,self.G,self.outedges)
+        if np.isnan(obs["EdgeFeat(Branchflow)"]).astype(np.int32).sum() > 0:
+            return self.reset()     
+        return obs
+
+
+    def render(self, mode='human', close=False):
+        pass
+           
--- train.py
+++ train.py
@@ -2,98 +2,22 @@ import numpy as np
 import gym
 from stable_baselines3 import PPO
 # from stable_baselines.common import make_vec_env
-from DSS_OutCtrl_Env import DSS_OutCtrl_Env
+from Environments.Bus_13.DSS_OutCtrl_Env import DSS_OutCtrl_Env
 # import json
 # import datetime as dt
 import torch
 # from stable_baselines3.common.utils import set_random_seed
-# from feedforwardPolicy import *
 from stable_baselines3 import A2C, PPO
-#from CustomPolicies import ActorCriticGCAPSPolicy
-from DSS_Initialize import   *
-from CustomPolicies import ActorCriticGCAPSPolicy
+from stable_baselines3.common.callbacks import CheckpointCallback, BaseCallback
+from Policies.CustomPolicies import ActorCriticGCAPSPolicy
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
 import pickle
 from stable_baselines3.common.utils import set_random_seed
-
+from Policies.Feature_Extractor import CustomGNN
+from Configs.training_config import get_training_config
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 
-
-class CustomGNN(BaseFeaturesExtractor):
-    """
-    :param observation_space: (gym.Space)
-    :param features_dim: (int) Number of features extracted.
-        This corresponds to the number of unit for the last layer.
-    """
-
-    def __init__(self,
-                observation_space: gym.spaces.Box,
-                 features_dim: int = 256,
-                 n_layers=2,
-                 n_dim=256,
-                 n_p=1,
-                 node_dim=3,
-                 n_K=1,
-                 ):
-        super(CustomGNN, self).__init__(observation_space, features_dim)
-        self.n_layers = n_layers
-        self.n_dim = n_dim
-        self.n_p = n_p
-        self.n_K = n_K
-        self.node_dim = node_dim
-        self.init_embed = torch.nn.Linear(node_dim, n_dim * n_p)
-        self.init_embed_depot = torch.nn.Linear(2, n_dim)
-
-        self.W_L_1_G1 = torch.nn.Linear(n_dim * (n_K + 1) * n_p, n_dim)
-
-        self.normalization_1 = torch.nn.BatchNorm1d(n_dim * n_p)
-
-        self.W_F = torch.nn.Linear(n_dim * n_p, features_dim)
-        self.full_context_nn = torch.nn.Linear(19, 128)
-        self.switch_encoder = torch.nn.Linear(16, 128)
-
-        self.activ = torch.nn.LeakyReLU()
-
-    def forward(self, data):
-        X = data['NodeFeat(BusVoltage)']
-        num_samples, num_locations, _ = X.size()
-        A = data["Adjacency"]
-        # print(A.shape)
-        D = torch.mul(torch.eye(num_locations).expand((num_samples, num_locations, num_locations)).to(A.device),
-                      (A.sum(-1))[:, None].expand((num_samples, num_locations, num_locations)))
-
-        # Layer 1
-
-        # p = 3
-        F0 = self.init_embed(X)
-        # print(torch.isnan(X).to(torch.int32).sum())
-        #print(X)
-        # K = 3
-        L = D - A
-        
-        g_L1_1 = self.W_L_1_G1(torch.cat((F0[:, :, :],
-                                          torch.matmul(L, F0)[:, :, :]
-                                          ),
-                                         -1))
-
-        F1 = g_L1_1  # torch.cat((g_L1_1), -1)
-        # F1 = self.activ(F1)
-
-        F_final = self.W_F(F1)
-
-        h = F_final  # torch.cat((init_depot_embed, F_final), 1)
-        # return (
-        #     h,  # (batch_size, graph_size, embed_dim)
-        #     h.mean(dim=1),  # average to get embedding of graph, (batch_size, embed_dim)
-        # )
-        switch_embeddings = self.switch_encoder(h.permute(0,2,1))
-       
-        context = self.full_context_nn(torch.cat((data["EnergySupp"],data["VoltageViolation"], data["EdgeFeat(Branchflow)"]), -1))
-        
-        final = switch_embeddings.mean(dim=1)+context
-        
-        return final
 
 def learning_rate_schedule(initial_value: float) -> Callable[[float], float]:
 
@@ -118,53 +42,43 @@ def make_env(rank, seed=0):
         return env
     set_random_seed(seed)
     return _init
+
+
 if __name__ == '__main__':
-# env = openDSSenv34()
-    num_cpu = 4
-    use_cuda = torch.cuda.is_available() 
-    # print(use_cuda)
+
+    training_config = get_training_config()
+    num_cpu = training_config.num_cpu
+    use_cuda = training_config.use_cuda #torch.cuda.is_available() 
+    training_config.device = torch.device("cuda:0" if training_config.use_cuda else "cpu")
+    tb_logger_location = training_config.logger + training_config.node_encoder
+    save_prefix = training_config.model_save+training_config.node_encoder
+
+    checkpoint_callback = CheckpointCallback(save_freq=training_config.save_freq, save_path=training_config.model_save,
+                                         name_prefix=training_config.node_encoder)
     env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
 
-    # n_envs = 4
-    # env = make_vec_env(mTSPEnv, n_envs=n_envs, env_kwargs={"n_locations":21, "n_agents":5})
-
-    rms_prop_eps = 1e-5
     policy_kwargs = dict(
         features_extractor_class=CustomGNN,
-        features_extractor_kwargs=dict(features_dim=128,node_dim=3),
+        features_extractor_kwargs=dict(features_dim=training_config.features_dim,node_dim=3),
         #activation_fn=torch.nn.Sigmoid,
-        net_arch=[dict(pi=[128,128])],
-        device=torch.device("cuda:0" if use_cuda else "cpu")
+        net_arch=[dict(pi=[training_config.features_dim,training_config.features_dim])],
+        device=training_config.device
         # optimizer_class = th.optim.RMSprop,
         # optimizer_kwargs = dict(alpha=0.89, eps=rms_prop_eps, weight_decay=0)
     )
 
     model = PPO(
         policy=ActorCriticGCAPSPolicy, 
-        env=env,tensorboard_log="logger/", 
+        env=env,
+        tensorboard_log=tb_logger_location, 
         policy_kwargs=policy_kwargs, 
         verbose=1,
-        n_steps=10000,
-        batch_size=20000,
-        gamma=1.00,
-        learning_rate=learning_rate_schedule(0.000001),
-        ent_coef=0.00
-        ).learn(total_timesteps=1000000, log_interval=1, tb_log_name="s1")
+        n_steps=training_config.n_steps,
+        batch_size=training_config.batch_size,
+        gamma=training_config.gamma,
+        learning_rate=learning_rate_schedule(training_config.learning_rate),
+        ent_coef=training_config.ent_coef
+        ).learn(total_timesteps=training_config.total_steps, callback=checkpoint_callback)
 
 
-
-
-    # model = A2C(ActorCriticGCAPSPolicy, env,tensorboard_log="logger/", policy_kwargs=policy_kwargs, verbose=1, n_steps=200,
-    #         use_rms_prop=False,
-    #         gamma=1.00,
-    #         learning_rate=learning_rate_schedule(0.07),
-    #             ).learn(total_timesteps=20000, n_eval_episodes=1, log_interval=1, tb_log_name="R1_new_env")
-    # model = A2C('MultiInputPolicy', env, tensorboard_log="logger/", policy_kwargs=policy_kwargs, verbose=1,
-    #             n_steps=1).learn(total_timesteps=20000, n_eval_episodes=1)
-    # model.learn(total_timesteps=2000)
-
-
-# observations = env.test_func()
-
-    log_dir = "."
-    model.save("s1")
+    model.save(save_prefix+"_final")
